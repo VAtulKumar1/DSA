c83266a42da66cb2fad3689e606839a6f29d016f
VAtulKumar1 <atulkumarverma304@gmail.com>
2024-09-10 22:38:28 +0530

    peek method stream api

--- DSA in Java/src/StremApi/PeekMethod.java
+++ DSA in Java/src/StremApi/PeekMethod.java
@@ -5,6 +5,7 @@ import java.util.stream.Stream;
 public class PeekMethod {
 
     public static void main(String[] args) {
+        // to peek into stream in the middle of the operations
         Stream.of(12,2,3,3,4,45,54,5,5)
                 .peek(n-> System.out.println(n))
                 .distinct()
