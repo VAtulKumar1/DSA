5de2099025c69c67e03b1b7d62495a2dfacef1c3
VAtulKumar1 <atulkumarverma304@gmail.com>
2024-09-20 02:24:27 +0530

    Top K Frequent ELements

---
+++ DSA in Java/src/heap/TopK_FrequentElements.java
@@ -0,0 +1,46 @@
+package heap;
+
+import java.net.Inet4Address;
+import java.util.*;
+
+public class TopK_FrequentElements {
+
+    public static List<Integer> find(int[] nums,int k){
+        List<Integer> res = new ArrayList<>();
+        HashMap<Integer,Integer> map = new HashMap<>();
+        PriorityQueue<Map.Entry<Integer, Integer>> pq =
+                new PriorityQueue<>((a, b)-> a.getValue()==b.getValue()
+                        ?a.getValue():a.getValue()-b.getValue());
+
+        for (int num : nums){
+            if(map.containsKey(num)){
+                map.put(num , map.get(num) + 1);
+            }else{
+                map.put(num,1);
+            }
+
+        }
+
+        for(Map.Entry<Integer,Integer> entry: map.entrySet()){
+            pq.add(entry);
+            if(pq.size() > k){
+                pq.poll();
+            }
+
+        }
+
+        while(!pq.isEmpty()){
+            res.add(pq.poll().getKey());
+        }
+
+
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {6,6,6,6,6,5,5,5,5,5,1,1,1,1,1,1,3,2,2,2,2,2,2,2,2,2,2,8,10,9};
+        int k = 3;
+        List<Integer> res = find(nums,k);
+    }
+}
